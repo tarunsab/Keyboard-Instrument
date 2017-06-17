25f526474a63d07c9fc52fe378940e977c2574a9
Tarun Sabbineni <ts2615@imperial.ac.uk>
2017-06-17 09:42:42 +0100

    Initial Files

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import org.jfugue.player.Player;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        Player player = new Player();
+        player.play("C D E F G A B");
+
+//      System.out.println("Hello World");
+//
+//      Player player = new Player();
+//      char c = reader.next(".").charAt(0);
+//
+//      player.play("A C# E");
+
+    }
+}
